c285c4ed6d95d3c6a351453159e591e327d27913
= <=>
2015-07-03 09:10:40 -0400

    Extracted the vector functions in a separate file

---
+++ vector.js
@@ -0,0 +1,1 @@
+function vector() {	return { x: 0, y: 0 };}function vector(x, y) {	return { x: x, y: y};}function distance(v1, v2) {	var dx = Math.abs(v1.x - v2.x);	var dy = Math.abs(v1.y - v2.y);	return Math.sqrt(dx*dx + dy*dy);}function add(v1, v2) {	return vector(v1.x+v2.x, v1.y+v2.y);}
