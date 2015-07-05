cbab6f13a1d409a9e836ac2e43520804cfb8ef72
unknown <f_roseberry@live.fr>
2015-07-05 14:58:53 -0400

    Refactored the boid class

--- boid.js
+++ boid.js
@@ -6,7 +6,7 @@ function boid(position, velocity) {
 		x: function () { return this._position.x(); },
 		y: function () { return this._position.y(); },
 		update: function (boids, params) {
-			var acceleration = this.flock(boids, params);
+			var acceleration = flock(this, boids, params);
 			this._velocity = this._velocity.add(acceleration).clamp(params.maxSpeed);
 		
 			if (this._position.y() + this._velocity.y() <= 1) { // Will exit by the top
@@ -28,86 +28,88 @@ function boid(position, velocity) {
 			} else {
 				this._position.set_x(this._position.x() + this._velocity.x());
 			}
-		},
-		flock: function (neighboors, params) {
-			this.separation = this.separate(neighboors, params).multiply(params.weights.separation);
-			this.alignment = this.align(neighboors, params).multiply(params.weights.alignment);
-			this.cohesion = this.cohere(neighboors, params).multiply(params.weights.cohesion);
-			
-			return this.separation.add(this.alignment).add(this.cohesion);
-		},
-		separate: function (neighboors, params) {
-			var mean = vector(0, 0);
-			var count = 0;
-			var self = this;
-			_.each(neighboors, function (neighboor) {
-				var d = neighboor._position.distance(self._position);
-				if (d > 0 && d < params.desiredSeparation) {
-					var localMean = self._position.subtract(neighboor._position).normalize().divide(d);
-					mean = mean.add(localMean);
-					count += 1;
-				}
-			});
-			
-			if (count > 0) {
-				return mean.divide(count);
-			}
-			
-			return mean;
-		},
-		align: function (neighboors, params) {
-			var mean = vector(0, 0);
-			var count = 0;
-			var self = this;
-			_.each(neighboors, function (neighboor) {
-				var d = neighboor._position.distance(self._position);
-				if (d > 0 && d < params.neighboorRadius) {
-					mean = mean.add(neighboor._velocity);
-					count += 1;
-				}
-			});
-			
-			if (count > 0) {
-				return mean.divide(count).clamp(params.maxForce);
-			}
-			
-			return mean;
-		},
-		cohere: function (neighboors, params) {
-			var sum = vector(0, 0);
-			var count = 0;
-			var self = this;
-			_.each(neighboors, function (neighboor) {
-				var d = neighboor._position.distance(self._position);
-				if (d > 0 && d < params.neighboorRadius) {
-					sum = sum.add(neighboor._position);
-					count += 1;
-				}
-			});
-			
-			if (count > 0) {
-				return this.steer_to(sum.divide(count), params);
-			}
-			
-			return sum;
-		},
-		steer_to: function (target, params) {
-			var desired = target.subtract(this._position);
-			var d = desired.magnitude();
-			
-			if (d > 0) {
-				desired = desired.normalize();
-				
-				if (d < 100.0) {
-					desired = desired.multiply(params.maxSpeed * (d / 100.0));
-				} else {
-					desired = desired.multiply(params.maxSpeed);
-				}
-				
-				return desired.subtract(this._velocity).clamp(params.maxForce);
-			}
-			
-			return vector(0, 0);
 		}
 	};
+	
+	function flock (self, neighboors, params) {
+		self.separation = separate(self, neighboors, params).multiply(params.weights.separation);
+		self.alignment = align(self, neighboors, params).multiply(params.weights.alignment);
+		self.cohesion = cohere(self, neighboors, params).multiply(params.weights.cohesion);
+			
+		return self.separation.add(self.alignment).add(self.cohesion);
+	}
+	
+	function separate (self, neighboors, params) {
+		var mean = vector(0, 0);
+		var count = 0;
+		_.each(neighboors, function (neighboor) {
+			var d = neighboor._position.distance(self._position);
+			if (d > 0 && d < params.desiredSeparation) {
+				var localMean = self._position.subtract(neighboor._position).normalize().divide(d);
+				mean = mean.add(localMean);
+				count += 1;
+			}
+		});
+		
+		if (count > 0) {
+			return mean.divide(count);
+		}
+		
+		return mean;
+	}
+	
+	function align (self, neighboors, params) {
+		var mean = vector(0, 0);
+		var count = 0;
+		_.each(neighboors, function (neighboor) {
+			var d = neighboor._position.distance(self._position);
+			if (d > 0 && d < params.neighboorRadius) {
+				mean = mean.add(neighboor._velocity);
+				count += 1;
+			}
+		});
+		
+		if (count > 0) {
+			return mean.divide(count).clamp(params.maxForce);
+		}
+		
+		return mean;
+	}
+	
+	function cohere (self, neighboors, params) {
+		var sum = vector(0, 0);
+		var count = 0;
+		_.each(neighboors, function (neighboor) {
+			var d = neighboor._position.distance(self._position);
+			if (d > 0 && d < params.neighboorRadius) {
+				sum = sum.add(neighboor._position);
+				count += 1;
+			}
+		});
+		
+		if (count > 0) {
+			return steer_to(self, sum.divide(count), params);
+		}
+		
+		return sum;
+	}
+	
+	function steer_to (self, target, params) {
+		var desired = target.subtract(self._position);
+		var d = desired.magnitude();
+		
+		if (d > 0) {
+			desired = desired.normalize();
+			
+			if (d < 100.0) {
+				desired = desired.multiply(params.maxSpeed * (d / 100.0));
+			} else {
+				desired = desired.multiply(params.maxSpeed);
+			}
+			
+			return desired.subtract(self._velocity).clamp(params.maxForce);
+		}
+		
+		return vector(0, 0);
+	}
 }
