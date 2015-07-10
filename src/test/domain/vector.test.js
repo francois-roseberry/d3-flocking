cb7d1bd25442a42159297b1c69e09ac316ddc8aa
François Roseberry <f_roseberry@live.fr>
2015-07-10 14:42:23 -0400

    Update vector.test.js
    Corrected an 'it' message

--- src/test/domain/vector.test.js
+++ src/test/domain/vector.test.js
@@ -7,7 +7,7 @@
 	describe('A unit vector', function () {
 		var vector = Vector.newVector(1, 0);
 		
-		it('should have a magnitude of 1', function () {
+		it('has a magnitude of 1', function () {
 			expect(vector.magnitude()).to.be(1);
 		});
 	});
