2d045fc96329b15f16cf85ddf676fc90587fbf60
unknown <f_roseberry@live.fr>
2015-07-25 21:06:28 -0400

    Reorganizing the grunt file

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -4,107 +4,152 @@ var karmaRunner = require('./build/karma_runner');
 var buildServices = require('./build/build-services.js');
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-	jshint: {
-		options: {
-			jshintrc: "build/jshintrc"
-		},
-		all: {
-			src: [
-				'Gruntfile.js',
-				'src/**/*.js'
-			]
-		}
-	},
 	
-	csslint: {
-		options: {
-			csslintrc: 'build/csslintrc'
-		},
-		all: {
-			src: ['src/**/*.css']
-		}
-	},
+	// Default task, do everything
+	grunt.registerTask('default', 'Build and test everything', ['lint', 'package', 'test']);
 	
-	concat: {
-		dependencies: {
-			src: [
-				'node_modules/underscore/underscore.js',
-				'node_modules/d3/d3.min.js',
-				'node_modules/rx/dist/rx.all.min.js',
-				'node_modules/jquery/dist/jquery.min.js',
-				'node_modules/jquery-ui/jquery-ui.js'
-			],
-			dest: 'target/dist/dependencies.js'
-		}
-	},
-		
-	copy: {
-		flattenSourceAndTest: {
-			expand: true,
-			src: ['src/main/**/*.js',
-				'src/test/unit/**/*.js',
-				'src/test/integration/**/*.js'],
-			dest: 'target/stagger/src',
-			filter: 'isFile',
-			flatten: true
+	// Quick build
+	grunt.registerTask('check', ['lint', 'package', 'runTest']);
+	
+	// Code quality
+	grunt.registerTask('lint', ['jshint', 'csslint']);
+	grunt.registerTask('test', ['service', 'runTest']);
+	grunt.registerTask('runTest', ['karmaTest', 'mochaTest']);
+
+	grunt.registerTask('karmaTest', 'Run the client unit test in all browser connected to Karma', function () {
+	  karmaRunner.runTests(grunt, this.async());
+	});
+	
+	// Packaging
+	grunt.registerTask('prepare', ['copy:html', 'copy:flattenSourceAndTest', 'copy:flattenSmokeTest']);
+	grunt.registerTask('copySource', ['copy:jqueryUiImages']);
+	grunt.registerTask('minify', ['cssmin']);
+	grunt.registerTask('package', ['prepare', 'browserify', 'concat', 'copySource', 'minify']);
+	
+	// Background services
+	grunt.registerTask('background', ['service', 'wait']);
+	grunt.registerTask('service', ['karmaServer', 'simpleServer', 'webdriver']);
+  
+	grunt.registerTask('simpleServer', function () {
+	  buildServices.startServer(this.async());
+	});
+
+	grunt.registerTask('karmaServer', function () {
+	  buildServices.startKarmaServer(this.async());
+	});
+
+	grunt.registerTask('webdriver', 'Start PhantomJS in WebDriver mode', function () {
+	  buildServices.startPhantomJsWebdriver(this.async());
+	});
+
+	grunt.registerTask('wait', 'wait until the exit, useful for background services', function () {
+	  grunt.log.writeln("Background service started".green);
+	  grunt.log.writeln("Run 'grunt check' to do a quick build while this process is running");
+	  this.async();
+	});
+	
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: "build/jshintrc"
+			},
+			all: {
+				src: [
+					'Gruntfile.js',
+					'build/**/*.js',
+					'src/**/*.js'
+				]
+			}
 		},
 
-		flattenSmokeTest: {
-			expand: true,
-			src: ['src/test/smoke/**/*.js',
-				'src/test/page-object/**/*.js'],
-			dest: 'target/stagger/smoke',
-			filter: 'isFile',
-			flatten: true
-		},
-		
-		html: {
-			src: ['src/static/index.html'],
-			dest: 'target/dist/index.html',
-			filter: 'isFile'
-		},
-		
-		jqueryUiImages: {
-			expand: true,
-			cwd: 'node_modules/jquery-ui/themes/vader/images/',
-			src: ['**'],
-			dest: 'target/dist/images/',
-			filter: 'isFile'
-		}
-	},
-		
-	browserify: {
-		dist: {
-			files: {
-				'./target/dist/app.js': ['target/stagger/src/bootstrap.js']
+		csslint: {
+			options: {
+				csslintrc: 'build/csslintrc'
+			},
+			all: {
+				src: ['src/**/*.css']
 			}
-		}
-	},
-	
-	mochaTest: {
-		smoke: {
-                options: {
-                    reporter: 'list'
-                },
+		},
+		
+		browserify: {
+			dist: {
+				files: {
+					'./target/dist/app.js': ['target/stagger/src/bootstrap.js']
+				}
+			}
+		},
 
-                src: ['target/stagger/smoke/*']
-            }
-    },
-	
-	cssmin: {
-		target: {
-			files: {
-				'target/dist/styles.min.css': [
-												'src/**/*.css',
-												'node_modules/jquery-ui/themes/vader/jquery-ui.min.css'
-											]
+		concat: {
+			dependencies: {
+				src: [
+					'node_modules/underscore/underscore.js',
+					'node_modules/d3/d3.min.js',
+					'node_modules/rx/dist/rx.all.min.js',
+					'node_modules/jquery/dist/jquery.min.js',
+					'node_modules/jquery-ui/jquery-ui.js'
+				],
+				dest: 'target/dist/dependencies.js'
 			}
-		}
-	},
-	
-	clean: ['target/**'],
-  });
+		},
+			
+		copy: {
+			flattenSourceAndTest: {
+				expand: true,
+				src: ['src/main/**/*.js',
+					'src/test/unit/**/*.js',
+					'src/test/integration/**/*.js'],
+				dest: 'target/stagger/src',
+				filter: 'isFile',
+				flatten: true
+			},
+
+			flattenSmokeTest: {
+				expand: true,
+				src: ['src/test/smoke/**/*.js',
+					'src/test/page-object/**/*.js'],
+				dest: 'target/stagger/smoke',
+				filter: 'isFile',
+				flatten: true
+			},
+			
+			html: {
+				src: ['src/static/index.html'],
+				dest: 'target/dist/index.html',
+				filter: 'isFile'
+			},
+			
+			jqueryUiImages: {
+				expand: true,
+				cwd: 'node_modules/jquery-ui/themes/vader/images/',
+				src: ['**/*'],
+				dest: 'target/dist/images/',
+				filter: 'isFile'
+			}
+		},
+		
+		mochaTest: {
+			smoke: {
+				options: {
+					reporter: 'list'
+				},
+
+				src: ['target/stagger/smoke/*']
+			}
+		},
+
+		cssmin: {
+			target: {
+				files: {
+					'target/dist/styles.min.css': [
+													'src/**/*.css',
+													'node_modules/jquery-ui/themes/vader/jquery-ui.min.css'
+												]
+				}
+			}
+		},
+
+		clean: ['target/**'],
+	});
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -114,38 +159,4 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  
-  grunt.registerTask('lint', ['jshint', 'csslint']);
-  grunt.registerTask('minify', ['cssmin']);
-  grunt.registerTask('package', ['prepare', 'browserify', 'concat', 'copySource', 'minify']);
-  grunt.registerTask('prepare', ['copy:html', 'copy:flattenSourceAndTest', 'copy:flattenSmokeTest']);
-  grunt.registerTask('copySource', ['copy:jqueryUiImages']);
-  grunt.registerTask('test', ['karmaTest', 'mochaTest']);
-  grunt.registerTask('check', ['lint', 'package', 'test']);
-  grunt.registerTask('background', ['service', 'wait']);
-  grunt.registerTask('service', ['karmaServer', 'simpleServer', 'webdriver']);
-  
-  grunt.registerTask('simpleServer', function () {
-	  buildServices.startServer(this.async());
-  });
-  
-  grunt.registerTask('karmaServer', function () {
-	  buildServices.startKarmaServer(this.async());
-  });
-  
-  grunt.registerTask('webdriver', 'Start PhantomJS in WebDriver mode', function () {
-      buildServices.startPhantomJsWebdriver(this.async());
-  });
-  
-  grunt.registerTask('wait', 'wait until the exit, useful for background services', function () {
-	  grunt.log.writeln("Background service started".green);
-	  grunt.log.writeln("Run 'grunt check' to do a quick build while this process is running");
-	  this.async();
-  });
-
-  grunt.registerTask('karmaTest', 'Run the client unit test in all browser connected to Karma', function () {
-      karmaRunner.runTests(grunt, this.async());
-  });
-  
-  grunt.registerTask('default', ['lint', 'package']);
 };
